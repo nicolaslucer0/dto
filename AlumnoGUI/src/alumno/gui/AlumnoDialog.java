5f296ba8022543d7b71c05feff60e0176c3eff7f
Nicolas Lucero <nicolas.lucero@redb.ee>
2022-06-26 19:56:54 -0300

    sql validation change

--- AlumnoGUI/src/alumno/gui/AlumnoDialog.java
+++ AlumnoGUI/src/alumno/gui/AlumnoDialog.java
@@ -247,34 +247,8 @@ public class AlumnoDialog extends javax.swing.JDialog {
             evt.consume();
     }//GEN-LAST:event_apynTextFieldKeyTyped
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AlumnoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AlumnoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AlumnoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AlumnoDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 AlumnoDialog dialog = new AlumnoDialog(new javax.swing.JFrame(), true);
@@ -289,11 +263,6 @@ public class AlumnoDialog extends javax.swing.JDialog {
         });
     }
 
-    /**
-     *
-     * @param alu Si en null ==> se agrega un alumno - si no, es
-     * consultar/modificar
-     */
     Alumno mostrar(Alumno alu, boolean isUpdate) throws PersonaException, AlumnoException {
         if (alu == null) {
             changeStateComboBox(false);
